e2803983978e3df529942fbb3672e94a474411b5
Dan Underwood <dunderwood@advancedbasics.com>
2024-06-20 21:33:42 +0000

    Start testing app:api

--- apps/api/src/routes/v1.routes.spec.ts
+++ apps/api/src/routes/v1.routes.spec.ts
@@ -1,14 +1,38 @@
+import { Express as IExpress} from 'express';
+import { Express } from 'jest-express/lib/express';
 import { RoutesV1 } from './v1.routes';
 
+let app: IExpress;
+
 describe('RoutesV1', () => {
-  // arrange
-  // const routes = new RoutesV1();
-  // act
-  // assert
+  beforeEach(() => {
+    app = new Express() as unknown as IExpress;
+  });
+
+  // @ts-expect-error - type error
+  afterEach(() => app.resetMocked());
+
   it('should exist when imported', () => {
     expect(RoutesV1).toBeDefined();
   });
-  // it('should have a method to loadRoutes', () => {
-  //   expect(routes.loadRoutes).toBeDefined();
-  // });
+
+  it('should have required methods and properties when instantiated', () => {
+    // arrange
+    const routes = new RoutesV1(app);
+    // act
+    // assert
+    expect(routes.loadRoutes).toBeDefined();
+    expect(routes.app).toBeDefined();
+    expect(routes.healthzRoutes).toBeDefined();
+    expect(routes.userRoutes).toBeDefined();
+  });
+
+  it('should load routes when invoked', () => {
+    // arrange
+    const routes = new RoutesV1(app);
+    // act
+    routes.loadRoutes();
+    // assert
+    expect(app.use).toHaveBeenCalledTimes(3);
+  });
 });
