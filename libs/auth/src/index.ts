cd1587ee0545ae5ad83517c691c6bf8b7cae40e1
Dan Underwood <dunderwood@advancedbasics.com>
2024-06-22 19:53:23 +0000

    Added middlewares for auth. Testing for ensure-logged-in

--- libs/auth/src/index.ts
+++ libs/auth/src/index.ts
@@ -6,3 +6,9 @@ export {
   TokenService,
   TokenServiceType
 } from './api/token.service';
+export { ensureLoggedIn } from './api/ensure-logged-in.middleware';
+export {
+  hasAdminRole,
+  hasSuperAdminRole,
+  userHasRole
+} from './api/ensure-role.middleware';
--- libs/user/src/__mocks__/index.ts
+++ libs/user/src/__mocks__/index.ts
@@ -1,5 +1,38 @@
 import { Model } from "sequelize-typescript";
+import { USER_ROLE } from "../model/user.consts";
+
+const mockUsers = {
+  user1: {
+    roles: [
+      USER_ROLE.SUPER_ADMIN,
+      USER_ROLE.ADMIN,
+      USER_ROLE.USER
+    ]
+  },
+  user2: {
+    roles: [
+      USER_ROLE.ADMIN,
+      USER_ROLE.USER
+    ]
+  },
+  user3: {
+    roles: [
+      USER_ROLE.USER
+    ]
+  }
+};
 
 export class UserModel extends Model<UserModel> {
   id: string;
+
+  static async userHasRole(id: string, role: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      const user = mockUsers[id];
+      if (!user) {
+        reject('no user with this id');
+      }
+
+      return user.roles.indexOf(role) > -1;
+    });
+  }
 }
